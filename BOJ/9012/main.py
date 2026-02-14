3a26d993a65dfea4c9de3a672c6056c4746d3400
Gyeongho Kim <62329114+Ahpix@users.noreply.github.com>
2026-02-14 22:02:21 +0900

    Refactor: remove isChecked using for-else structure

--- BOJ/9012/main.py
+++ BOJ/9012/main.py
@@ -1,23 +1,18 @@
 n = int(input())
-isChecked = False
 
 for i in range(n):
     stack = []
     ps = input()
-    isChecked = False
     for j in ps:
         if j == "(":
             stack.append(j)
         else:
             if not stack:
                 print("NO")
-                isChecked = True
                 break
             stack.pop()       
-    if stack:
-        print("NO")
     else:
-        if isChecked:
-            continue
+        if stack:
+            print("NO")
         else:
             print("YES")
